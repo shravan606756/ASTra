aec03a65163db7cb36946e00e7f6f90d0f814c67
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-07-17 18:55:38 +0530

    added project parser to scan project recursively

--- jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/parser/JavaProjectParser.java
+++ jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/parser/JavaProjectParser.java
@@ -1,4 +1,36 @@
 package com.shravan.jcode_intelligence.parser;
 
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class JavaProjectParser {
+
+    public List<CompilationUnit> parseProject(Path projectRoot) throws IOException {
+
+        List<CompilationUnit> units = new ArrayList<>();
+
+        Files.walk(projectRoot)
+                .filter(Files::isRegularFile)
+                .filter(path -> path.toString().endsWith(".java"))
+                .forEach(path -> {
+
+                    try {
+                        units.add(StaticJavaParser.parse(path));
+                    }
+                    catch (Exception e) {
+                        System.out.println("Failed to parse : " + path);
+                    }
+
+                });
+
+        return units;
+    }
 }

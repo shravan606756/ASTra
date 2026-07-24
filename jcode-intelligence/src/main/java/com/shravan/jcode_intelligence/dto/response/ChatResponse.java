3acb14ef3984cef64717fa42ad021551f67aeb62
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-07-24 17:54:10 +0530

    improve chat response model

--- jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/dto/response/ChatResponse.java
+++ jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/dto/response/ChatResponse.java
@@ -1,4 +1,44 @@
 package com.shravan.jcode_intelligence.dto.response;
 
+import java.util.List;
+
 public class ChatResponse {
+
+    private String query;
+    private String answer;
+    private List<ChunkResponse> sources;
+
+    public ChatResponse() {
+    }
+
+    public ChatResponse(String query, String answer, List<ChunkResponse> sources) {
+        this.query = query;
+        this.answer = answer;
+        this.sources = sources;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public List<ChunkResponse> getSources() {
+        return sources;
+    }
+
+    public void setSources(List<ChunkResponse> sources) {
+        this.sources = sources;
+    }
 }
+

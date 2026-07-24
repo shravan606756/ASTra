94276de545e5bd63ee044da995cec70ab602f993
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-07-24 17:46:30 +0530

    update parser test runner

--- jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/ParserTestRunner.java
+++ jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/ParserTestRunner.java
@@ -1,12 +1,17 @@
 package com.shravan.jcode_intelligence;
 
 import com.shravan.jcode_intelligence.service.IndexingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("dev-runner")
 public class ParserTestRunner implements CommandLineRunner {
 
+    private static final Logger log = LoggerFactory.getLogger(ParserTestRunner.class);
     private final IndexingService indexingService;
 
     public ParserTestRunner(IndexingService indexingService) {
@@ -15,18 +20,11 @@ public class ParserTestRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
         String projectPath =
                 "C:\\Users\\Shravan\\OneDrive\\Desktop\\jcode-intelligence\\jcode-intelligence\\src\\main\\java";
 
-        System.out.println("\n==============================");
-        System.out.println("STARTING INDEXING");
-        System.out.println("==============================");
-
-        indexingService.indexProject(projectPath);
-
-        System.out.println("\n==============================");
-        System.out.println("INDEXING FINISHED");
-        System.out.println("==============================");
+        log.info("STARTING DEV RUNNER INDEXING for path: {}", projectPath);
+        int count = indexingService.indexProject(projectPath);
+        log.info("DEV RUNNER INDEXING FINISHED. Indexed {} documents.", count);
     }
-}
+}

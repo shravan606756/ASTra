b83fecdbe485f861cc5d2ad12657d97bd9f3d1eb
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-07-24 17:50:40 +0530

    add chat mode to request DTO

--- jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/dto/request/ChatRequest.java
+++ jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/dto/request/ChatRequest.java
@@ -1,4 +1,57 @@
 package com.shravan.jcode_intelligence.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class ChatRequest {
+
+    @NotBlank(message = "Query must not be blank")
+    private String query;
+
+    private Integer topK;
+
+    private String repositoryId;
+
+    private ChatMode mode;
+
+    public ChatRequest() {
+    }
+
+    public ChatRequest(String query, Integer topK, String repositoryId) {
+        this.query = query;
+        this.topK = topK;
+        this.repositoryId = repositoryId;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public Integer getTopK() {
+        return topK != null ? topK : 5;
+    }
+
+    public void setTopK(Integer topK) {
+        this.topK = topK;
+    }
+
+    public String getRepositoryId() {
+        return repositoryId;
+    }
+
+    public void setRepositoryId(String repositoryId) {
+        this.repositoryId = repositoryId;
+    }
+
+    public ChatMode getMode() {
+        return mode != null ? mode : ChatMode.AUTO;
+    }
+
+    public void setMode(ChatMode mode) {
+        this.mode = mode;
+    }
 }
+

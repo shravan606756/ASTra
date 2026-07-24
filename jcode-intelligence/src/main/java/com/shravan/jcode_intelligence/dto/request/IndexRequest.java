fe61c921e4377943a5f0c585753fdf9a7838bbd1
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-07-24 17:51:35 +0530

    feat: support Git repository indexing request

--- jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/dto/request/IndexRequest.java
+++ jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/dto/request/IndexRequest.java
@@ -1,4 +1,42 @@
 package com.shravan.jcode_intelligence.dto.request;
 
 public class IndexRequest {
+
+    private String projectPath;
+    private String gitUrl;
+    private String repositoryId;
+
+    public IndexRequest() {
+    }
+
+    public IndexRequest(String projectPath, String gitUrl, String repositoryId) {
+        this.projectPath = projectPath;
+        this.gitUrl = gitUrl;
+        this.repositoryId = repositoryId;
+    }
+
+    public String getProjectPath() {
+        return projectPath;
+    }
+
+    public void setProjectPath(String projectPath) {
+        this.projectPath = projectPath;
+    }
+
+    public String getGitUrl() {
+        return gitUrl;
+    }
+
+    public void setGitUrl(String gitUrl) {
+        this.gitUrl = gitUrl;
+    }
+
+    public String getRepositoryId() {
+        return repositoryId;
+    }
+
+    public void setRepositoryId(String repositoryId) {
+        this.repositoryId = repositoryId;
+    }
 }
+

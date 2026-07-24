96ac70c2781483a6c68ade07736576620a0197c3
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-07-24 17:59:48 +0530

    support mode-aware chat service

--- jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/service/ChatService.java
+++ jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/service/ChatService.java
@@ -1,4 +1,10 @@
 package com.shravan.jcode_intelligence.service;
 
+import com.shravan.jcode_intelligence.dto.request.ChatRequest;
+import com.shravan.jcode_intelligence.dto.response.ChatResponse;
+
 public interface ChatService {
+
+    ChatResponse answer(ChatRequest request);
 }
+

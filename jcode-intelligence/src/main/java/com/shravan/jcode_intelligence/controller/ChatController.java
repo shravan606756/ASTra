a509ae857a598a79b3fdfc2442eb1555baaa0abd
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-07-24 17:47:06 +0530

    extend chat controller with mode support

--- jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/controller/ChatController.java
+++ jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/controller/ChatController.java
@@ -1,4 +1,29 @@
 package com.shravan.jcode_intelligence.controller;
 
+import com.shravan.jcode_intelligence.dto.request.ChatRequest;
+import com.shravan.jcode_intelligence.dto.response.ChatResponse;
+import com.shravan.jcode_intelligence.service.ChatService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/chat")
 public class ChatController {
+
+    private final ChatService chatService;
+
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ChatResponse> chat(@Valid @RequestBody ChatRequest request) {
+        ChatResponse response = chatService.answer(request);
+        return ResponseEntity.ok(response);
+    }
 }
+

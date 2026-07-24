5c0b3329e53591c2b4a531e23976b63de60ce8d1
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-07-24 17:55:30 +0530

    return indexing statistics in response

--- jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/dto/response/IndexResponse.java
+++ jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/dto/response/IndexResponse.java
@@ -1,4 +1,52 @@
 package com.shravan.jcode_intelligence.dto.response;
 
 public class IndexResponse {
+
+    private String status;
+    private String message;
+    private int indexedChunksCount;
+    private String repositoryId;
+
+    public IndexResponse() {
+    }
+
+    public IndexResponse(String status, String message, int indexedChunksCount, String repositoryId) {
+        this.status = status;
+        this.message = message;
+        this.indexedChunksCount = indexedChunksCount;
+        this.repositoryId = repositoryId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getIndexedChunksCount() {
+        return indexedChunksCount;
+    }
+
+    public void setIndexedChunksCount(int indexedChunksCount) {
+        this.indexedChunksCount = indexedChunksCount;
+    }
+
+    public String getRepositoryId() {
+        return repositoryId;
+    }
+
+    public void setRepositoryId(String repositoryId) {
+        this.repositoryId = repositoryId;
+    }
 }
+

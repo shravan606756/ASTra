a06526947d7f060e589f3ece44593c9f07ba5d86
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-08-06 23:31:25 +0530

    Include a fun bunny renderer for a bit of personality in the UI

--- jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/cli/ui/BunnyRenderer.java
+++ jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/cli/ui/BunnyRenderer.java
@@ -1,4 +1,12 @@
 package com.shravan.jcode_intelligence.cli.ui;
 
 public class BunnyRenderer {
+    public static void render() {
+        System.out.println(ColorPalette.MUTED + "        (\\_/)" + ColorPalette.RESET);
+        System.out.println(ColorPalette.MUTED + "        (•ᴗ•)" + ColorPalette.RESET);
+        System.out.println(ColorPalette.MUTED + "        / >\uD83D\uDCDA" + ColorPalette.RESET);
+        System.out.println();
+        System.out.println("🐇 " + ColorPalette.SUCCESS + "Ready." + ColorPalette.RESET);
+        System.out.println();
+    }
 }

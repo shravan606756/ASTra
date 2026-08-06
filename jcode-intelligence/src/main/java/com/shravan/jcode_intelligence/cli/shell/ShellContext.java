1313da664dc35b19888874fef16595f44a51331c
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-08-06 23:31:24 +0530

    Create the shell context to keep track of current state and variables

--- jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/cli/shell/ShellContext.java
+++ jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/cli/shell/ShellContext.java
@@ -1,4 +1,13 @@
 package com.shravan.jcode_intelligence.cli.shell;
 
 public class ShellContext {
+    private String currentRepository;
+
+    public String getCurrentRepository() {
+        return currentRepository;
+    }
+
+    public void setCurrentRepository(String currentRepository) {
+        this.currentRepository = currentRepository;
+    }
 }

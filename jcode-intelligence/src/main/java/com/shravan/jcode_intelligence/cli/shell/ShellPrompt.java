8cd387a01a40a751a2c01ae9eeff52902e84792d
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-08-06 23:31:25 +0530

    Design the prompt string that appears when waiting for user input

--- jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/cli/shell/ShellPrompt.java
+++ jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/cli/shell/ShellPrompt.java
@@ -1,4 +1,11 @@
 package com.shravan.jcode_intelligence.cli.shell;
 
 public class ShellPrompt {
+    public static String getPrompt(ShellContext context) {
+        if (context.getCurrentRepository() == null || context.getCurrentRepository().isEmpty()) {
+            return "astra>";
+        } else {
+            return context.getCurrentRepository() + ">";
+        }
+    }
 }

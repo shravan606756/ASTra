e433e6b0abacc3f4072c2778b3414e7275f0f2cf
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-08-06 23:31:27 +0530

    Add ansi utility class to handle terminal escape codes easily

--- jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/cli/util/Ansi.java
+++ jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/cli/util/Ansi.java
@@ -1,4 +1,13 @@
 package com.shravan.jcode_intelligence.cli.util;
 
 public class Ansi {
+    public static final String RESET = "\u001B[0m";
+    public static final String BOLD = "\u001B[1m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String RED = "\u001B[31m";
+    public static final String WHITE = "\u001B[37m";
+    public static final String GRAY = "\u001B[90m";
 }

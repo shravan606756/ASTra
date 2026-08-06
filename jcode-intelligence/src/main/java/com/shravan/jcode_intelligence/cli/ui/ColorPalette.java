488e35aa2c0ef9a24c7b198c2bb72e1dab8ecf05
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-08-06 23:31:25 +0530

    Define a custom color palette to make the terminal output pop

--- jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/cli/ui/ColorPalette.java
+++ jcode-intelligence/src/main/java/com/shravan/jcode_intelligence/cli/ui/ColorPalette.java
@@ -1,4 +1,15 @@
 package com.shravan.jcode_intelligence.cli.ui;
 
+import com.shravan.jcode_intelligence.cli.util.Ansi;
+
 public class ColorPalette {
+    public static final String PRIMARY = Ansi.CYAN;
+    public static final String SECONDARY = Ansi.BLUE;
+    public static final String SUCCESS = Ansi.GREEN;
+    public static final String WARNING = Ansi.YELLOW;
+    public static final String ERROR = Ansi.RED;
+    public static final String TEXT = Ansi.WHITE;
+    public static final String MUTED = Ansi.GRAY;
+    public static final String ACCENT = Ansi.BOLD + Ansi.CYAN;
+    public static final String RESET = Ansi.RESET;
 }
